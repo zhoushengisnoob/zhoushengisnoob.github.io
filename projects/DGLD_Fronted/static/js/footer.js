ff4d26648d8959d94f7e1104da398a41374740ee
zhanhui1999 <zhanhui@zju.edu.cn>
2022-07-06 14:32:24 +0800

    update html

--- projects/DGLD_Fronted/static/js/footer.js
+++ projects/DGLD_Fronted/static/js/footer.js
@@ -1,8 +1,18 @@
 var text = String.raw`<div class="container">
 <div class="row justify-content-center">
+    <div class="footer-team">
+        <h1>DGLD Team</h1>
+    </div>
+    <div class="footer-school">
+        <a>Zhejiang University</a>
+    </div>
+    <div class="footer-name">
+        <a>Sheng Zhou, Peng Zhang, Gaoming Yang, Mengcheng Fang, Jinke Shi, Zinan Guo, Hui Zhan, Haishuai Wang, Jiajun Bu</a>
+    </div>
     <div class="col-lg-10">
         <div class="copy-right mb-5 text-center text-muted">Copyright© 2020-2022 ZJUEagle All Rights Reserved</div>
     </div>
+    
 </div>
 </div>`
 document.getElementById("footer").innerHTML += text
--- projects/DGLD_Fronted/static/js/header.js
+++ projects/DGLD_Fronted/static/js/header.js
@@ -2,13 +2,13 @@ var text = String.raw`<nav class="navbar navbar-expand-lg fixed-top sticky" id="
 <div class="container-fluid custom-container">
     <div>
         <a class="navbar-brand logo text-uppercase" href="javascript:;">
-            <img src="static/image/logo_small.png" class="logo-light" alt="" height="48">
+            <img src="static/image/DGLD_logo.jpg" class="logo-light" alt="" height="60">
             <!-- <p>Eagle Lab</p> -->
             <!-- <img src="images/logo-light.png" class="logo-dark" alt="" height="28"> -->
         </a>
-        <a class="lab-project-name">
+        <!-- <a class="lab-project-name">
             DGLD
-        </a>
+        </a>-->
     </div>
 
 
@@ -28,13 +28,10 @@ var text = String.raw`<nav class="navbar navbar-expand-lg fixed-top sticky" id="
                 <a href="./leaderboard.html" class="nav-link ">Leaderboard</a>
             </li>
             <li class="nav-item dropdown dropdown-hover">
-                <a href="../../DGLD/doc/docstring_html/html/dgld.html" class="nav-link ">Docs</a>
+                <a href="../../DGLD/doc/docstring_html/html/dgld.html" target="_blank" class="nav-link ">Docs</a>
             </li>
             <li class="nav-item">
-                <a href="#pricing" class="nav-link">Paper</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://github.com/EagleLab-ZJU/DGLD" class="nav-link">Github</a>
+                <a href="https://github.com/EagleLab-ZJU/DGLD" target="_blank" class="nav-link">Github</a>
             </li>
             
         </ul>
